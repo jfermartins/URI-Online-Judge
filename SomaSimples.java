c5d5f8c9f1c23e5463e099f96446496680cab00d
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2019-01-23 23:28:23 -0200

    Add files via upload

---
+++ SomaSimples.java
@@ -0,0 +1,18 @@
+package uri;
+
+import java.util.Scanner;
+
+public class SomaSimples {
+
+	public static void main(String[] args) {
+
+	// Jane Fernanda Martins
+	
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int SOMA = A + B;
+		System.out.println("SOMA = " + SOMA);
+		in.close();
+	}
+}
